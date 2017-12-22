e14605f03c3ae21fba2cf9808442d42c3a5ee14f
Mervin D'Souza <mdsouza@mdsouza-fed.york.local>
2017-12-22 16:51:31 -0600

    Added tab msging to background & content

--- background.js
+++ background.js
@@ -1,8 +1,16 @@
 let i = 1;
+console.clear();
 console.log("Background.js: Background Called!");
 chrome.browserAction.onClicked.addListener(bgButtonClicked);
 
-function bgButtonClicked() {
+function bgButtonClicked(tab) {
     console.log("Background Button Clicked!\nTimes: " + i);
+    console.log(tab);
     i += 1;
+
+    let msg = {
+        txt: "Hello World!"
+    }
+
+    chrome.tabs.sendMessag(tab.id, msg);
 }
--- content.js
+++ content.js
@@ -1,6 +1,13 @@
-console.log("Content.js: Extention Called!")
+console.clear();
+console.log("Content.js: Extention Called!");
 
-let para = document.getElementsByTagName('p');
+let para = document.getElementsByTagName('h1');
 for (elmnt of para){
     elmnt.style['background-color'] = "Yellow";
+}
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(request, sender, sendResponse){
+    
 }
